287f832acaaddcefd97511a43d7f53431db1b3e3
skclsso25 <110643104+Rhosungchan@users.noreply.github.com>
2022-09-22 16:39:43 +0900

    20220922

---
+++ member_board/src/edu/kh/jdbc/board/model/service/CommentService.java
@@ -0,0 +1,12 @@
+package edu.kh.jdbc.board.model.service;
+
+import static edu.kh.jdbc.common.JDBCTemplate.*;
+
+import edu.kh.jdbc.board.model.dao.CommentDAO;
+
+public class CommentService {
+
+	// CommentDAO 객체 생성 
+	private CommentDAO dao = new CommentDAO();
+	
+}
